1f6d9b232646b248f9458873a39313b1a8147e04
benjaminstokes <stokesbo@gmail.com>
2020-08-22 18:53:34 -0400

    filenameupdate

--- scripts/install/common/install-cpp2015update3rc.ps1
+++ scripts/install/common/install-cpp2015update3rc.ps1
@@ -20,7 +20,7 @@ The installer file is determined in this order:
 
 param (
  [Parameter(Mandatory = $False)] [String] $installer,
- [Parameter(Mandatory = $False)] [String] $pattern = "vc_redist2015.x64*exe", # should have 1 wild card and end with file extension
+ [Parameter(Mandatory = $False)] [String] $pattern = "vc_redist.x64*exe", # should have 1 wild card and end with file extension
  [Parameter(Mandatory = $False)] [String] $expectedpath ="C:\programdata\checkmarx\automation\dependencies",
  [Parameter(Mandatory = $False)] [String] $s3prefix = "installation/common", 
  [Parameter(Mandatory = $False)] [String] $sourceUrl = "https://download.microsoft.com/download/0/6/4/064F84EA-D1DB-4EAA-9A5C-CC2F0FF6A638/vc_redist.x64.exe"
