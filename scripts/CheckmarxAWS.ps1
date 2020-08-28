1c6251d6c62768bf573c4e83a7fccedebd116064
benjaminstokes <stokesbo@gmail.com>
2020-08-28 00:15:04 -0400

    ssl refacotoring

--- scripts/CheckmarxAWS.ps1
+++ scripts/CheckmarxAWS.ps1
@@ -187,3 +187,374 @@ Class InstallerLocator {
     Write-Host "Finished downloading $($this.filename)"
   }
 }
+
+
+Class CheckmarxSystemInfo {
+  [String] $SystemManagerConfigFile
+  [String] $JobsManagerConfigFile
+  [String] $ScansManagerConfigFile
+  [String] $WebConfigFile
+  [String] $WebServicesConfigFile
+  [String] $RestAPIConfigFile
+  [String] $EngineConfigFile
+
+  [bool] $IsEngine
+  [bool] $IsWebPortal
+  [bool] $IsSystemManager 
+  [bool] $IsJobsManager 
+  [bool] $IsScansManager
+
+  CheckmarxSystemInfo() {
+    $this.SystemManagerConfigFile = ("{0}\bin\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx System Manager' -Name "Path" -ErrorAction SilentlyContinue), 'CxSystemManagerService.exe.config')
+    $this.JobsManagerConfigFile = ("{0}\bin\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Jobs Manager' -Name "Path" -ErrorAction SilentlyContinue), 'CxJobsManagerWinService.exe.config')
+    $this.ScansManagerConfigFile = ("{0}\bin\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Scans Manager' -Name "Path" -ErrorAction SilentlyContinue), 'CxScansManagerWinService.exe.config')
+    $this.WebConfigFile = ("{0}\web\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\CheckmarxWebPortal' -Name "Path" -ErrorAction SilentlyContinue), 'web.config')
+    $this.WebServicesConfigFile = ("{0}\CxWebInterface\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Web Services' -Name "Path" -ErrorAction SilentlyContinue), 'web.config')
+    $this.RestAPIConfigFile = ("{0}\CxRestAPI\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Web Services' -Name "PathRestAPI" -ErrorAction SilentlyContinue), 'web.config')
+    $this.EngineConfigFile = ("{0}\{1}" -f ( Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Engine Server' -Name 'Path' -ErrorAction SilentlyContinue), 'CxSourceAnalyzerEngine.WinService.exe.config')
+  
+    $this.IsEngine = ( !([String]::IsNullOrEmpty($this.EngineConfigFile)) -and (Test-Path -Path $this.EngineConfigFile))
+    $this.IsWebPortal = ( !([String]::IsNullOrEmpty($this.WebConfigFile)) -and (Test-Path -Path $this.WebConfigFile))
+    $this.IsSystemManager = ( !([String]::IsNullOrEmpty($this.isSystemManager)) -and (Test-Path -Path $this.isSystemManager))
+    $this.IsJobsManager = ( !([String]::IsNullOrEmpty($this.isJobsManager)) -and (Test-Path -Path $this.isJobsManager))
+    $this.IsScansManager = ( !([String]::IsNullOrEmpty($this.isScansManager)) -and (Test-Path -Path $this.isScansManager))
+  }
+}
+
+
+Class DateTimeUtil {
+  # Gets timestamp in UTC
+  [DateTime] NowUTC() {
+      return (Get-Date).ToUniversalTime()
+  }
+  static [String] GetFileNameTime() {
+    $timestamp = Get-Date -Format o | foreach { $_ -replace ":", "" }
+    $timestamp = $timestamp.Replace("-", "")
+    return $timestamp
+  }
+}
+
+Class Logger {
+  hidden [String] $componentName
+  hidden [DateTimeUtil] $dateUtil = [DateTimeUtil]::new()
+  hidden [string] $outfile = "C:\CheckmarxSetup.log"
+
+  Logger([String] $componentName) {
+    $this.componentName = $componentName
+    if (!(test-path $this.outfile)) {New-Item -ItemType "file" -Path $this.outfile}
+  }
+
+  Info([String] $message) {
+    $this.Log("INFO", $message)
+  }
+
+  Warn([String] $message) {
+    $this.Log("WARN", $message)
+  }
+
+  Error ([String] $message) {
+    $this.Log("ERROR", $message)
+  }
+
+  hidden Log([String] $level, [String] $message) {
+    [String] $enhancedMessage = $this.FormatLogMessage($level, $message)
+
+    [String] $color = "White"
+    if ($level -eq "WARN"){
+      $color = "Yellow"
+    } elseif ($level -eq "ERROR") {
+      $color = "Red"
+    }
+
+    $this.Console($enhancedMessage, $color)
+  }
+
+  hidden [String] FormatLogMessage ([String] $level, [String] $message) {
+    return $this.dateUtil.NowUTC().ToString("yyyy-MM-ddTHH:mm:ss.fff") + " " + $level.PadLeft(5, " ") + " [" + $this.componentName + "] : " + $message
+  }
+
+  hidden Console ([String] $message, $color) {
+    Write-Host $message -ForegroundColor $color
+    # Add-Content -Path $this.outfile -Value $message
+    $message | Out-File $this.outfile -Append -Width 1000
+  }
+}
+
+Class Base {
+  [Logger] $log = [Logger]::new($this.GetType().Fullname)
+}
+
+Class CxSASTEngineTlsConfigurer : Base {
+  hidden [String] $tlsPort
+  hidden [String] $thumbprint
+
+  CxSASTEngineTlsConfigurer([String] $thumbprint) {
+    $this.tlsPort = "443"
+  }
+  Configure() {
+     [CheckmarxSystemInfo] $cx = [CheckmarxSystemInfo]::new()
+     if ($cx.isEngine -eq $False) {
+       $this.log.Info("The engine was not found (no engine path), no configuration will take place")
+       return
+     }
+
+     if ([String]::IsNullOrWhiteSpace($cx.EngineConfigFile) -Or -Not [System.IO.File]::Exists($cx.EngineConfigFile)){
+       $this.log.Info("The engine config file was not found (no engine config file), no configuration will take place")
+       return
+     }
+
+    $this.ConfigureNetsh()
+    $this.ConfigureServiceConfigFile()
+    Restart-Service -Name CxScanEngine
+  }
+
+  hidden [String] GetIp() {
+    return (Get-NetIPConfiguration | Where-Object { $_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne "Disconnected"}).IPv4Address.Ipaddress
+  }
+
+  hidden runCmd($cmd, $arguments) {
+    $this.log.Info("running command: $cmd $arguments")
+    Start-Process "$cmd" -ArgumentList "$arguments" -Wait -NoNewWindow
+    $this.log.Info("finished running command: $cmd $arguments")
+  }
+
+  hidden ConfigureNetsh() {
+    # Delete any cert, then register our own
+    $appid = "{00112233-4455-6677-8899-AABBCCDDEEFF}"
+    $ipport = "0.0.0.0:$($this.tlsPort)"
+    $this.runCmd(("netsh.exe", "http delete sslcert ipport={0}" -f $ipport))
+    $this.runCmd(("netsh.exe", "http add sslcert ipport={0} certhash={1} appid={2}" -f $ipport, $($this.certificates.thumbprint) , $appid))
+    $this.runCmd("netsh.exe", "http add urlacl url=https://+:$($this.tlsPort)/CxSourceAnalyzerEngineWCF/CxEngineWebServices.svc user=`"NT AUTHORITY\NETWORK SERVICE`"")
+  }
+
+  hidden ConfigureServiceConfigFile() {
+    [CheckmarxSystemInfo] $cx = [CheckmarxSystemInfo]::new()
+    [Xml]$xml = Get-Content $cx.EngineConfigFile
+
+    $this.log.Info("Adding security mode = Transport")
+    $bindingNode = Select-Xml -xml $xml -XPath "/configuration/*/bindings/basicHttpBinding/binding"
+    $securityNode = $bindingNode.Node.SelectSingleNode('security')
+    if (!$securityNode){
+      $securityNode = $xml.CreateElement('security')
+      $bindingNode.Node.AppendChild($securityNode)  | Out-Null
+    }
+    $securityNode.SetAttribute("mode", "Transport")
+
+    $this.log.Info("Setting mexHttpsBinding")
+    $mexNode = $xml.SelectSingleNode("/configuration/*/services/service/endpoint[@address='mex']")
+    if (!$mexNode){
+      $this.log.Error("ERROR: \<endpoint address=""mex"" not found in $($cx.EngineConfigFile) - aborting configuration")
+      return
+    }
+    $mexNode.SetAttribute("binding", "mexHttpsBinding")
+    $this.log.Info("Configuring baseAddress")
+    $hostNode = $xml.SelectSingleNode("/configuration/*/services/service/host/baseAddresses/add")
+    if (!$hostNode){
+      $this.log.Error('ERROR: \<baseAddresses\>\<add\>... not found! - aborting configuration')
+      return
+    }
+
+    [string]$hostAddress = $hostNode.SelectSingleNode("@baseAddress").Value
+    $hostAddress = $hostAddress.Replace("http:", "https:").Replace(":80", ":" + $($this.tlsPort)).Replace("localhost", $($this.GetIp()))
+    $hostNode.SetAttribute("baseAddress", $hostAddress)
+
+    $serviceNode = $xml.SelectSingleNode("/configuration/*/behaviors/serviceBehaviors/behavior/serviceMetadata")
+    if (!$serviceNode){
+      $this.log.Error('ERROR: \<serviceMetadata\> not found! - aborting configuration')
+      return
+    }
+    $serviceNode.SetAttribute("httpsGetEnabled", "true")
+    $serviceNode.RemoveAttribute("httpGetEnabled")
+
+    $this.log.Info("saving $($cx.EngineConfigFile)")
+    $xml.Save($cx.EngineConfigFile)
+  }
+}
+
+Class CxManagerIisTlsConfigurer : Base {
+  hidden [String] $IISWebSite
+  hidden [String] $tlsPort
+  hidden [String] $thumbprint
+
+  CxManagerIISTlsConfigurer([String] $IISWebSite, [String] $tlsPort, [String] $thumbprint) {
+    $this.IISWebSite = $IISWebSite
+    $this.tlsPort = $tlsPort
+    $this.thumbprint = $thumbprint
+  }
+
+  Configure() {
+    [CheckmarxSystemInfo] $cx = [CheckmarxSystemInfo]::new()
+    if ($cx.isWebPortal -eq $False -and $cx.isSystemManager -eq $False) {
+      $this.log.Warn("The WEB or MANAGER component is not installed. Skipping IIS configuration")
+      return
+    }
+    $this.log.Info("Configuring IIS Bindings")
+    $this.log.info("Site: $($this.IISWebSite)")
+    $this.log.info("Port: $($this.tlsPort)")
+    $bindings = Get-WebBinding -Name $this.IISWebSite -Port $this.tlsPort
+    $this.log.info("Current binding: $bindings")
+    if ($bindings) {
+      $this.UpdateWebBinding()
+    } else {
+      $this.CreateWebBinding()
+    }
+
+    $this.log.Info("Restarting website...")
+    try {
+      Stop-Website -Name $($this.IISWebSite)
+    } catch {
+      $this.log.Error("Error stopping $($this.IISWebSite)")
+      $this.log.Error("Error message: $($_.Exception.Message)")
+      $this.log.Error("Error information: $($_)")
+    }
+
+    try {
+      Start-Website -Name $($this.IISWebSite)
+    } catch {
+      $this.log.Error("Error starting $($this.IISWebSite)")
+      $this.log.Error("Error message: $($_.Exception.Message)")
+      $this.log.Error("Error information: $($_)")
+    }
+  }
+
+  hidden UpdateWebBinding() {
+    $this.log.Info("Updating existing IIS Binding (delete, and add new)")
+    # Deleting and Binding new certificate
+    try {
+        $CxDeleteCertOut = $(cmd.exe /c "netsh.exe http delete sslcert ipport=0.0.0.0:$($this.tlsPort) 2>&1")
+        $this.log.info("Cmd output: $CxDeleteCertOut")
+        $CertBinding = Get-Item cert:\LocalMachine\My\$($this.thumbprint) | New-Item 0.0.0.0!$($this.tlsPort)
+    } catch {
+      $this.log.Error("An error occured while updating https binding in IIS")
+      $this.log.error("Error message: $($_.Exception.Message))")
+      $this.log.Error("Error exception: $($_.Exception))")
+      Throw $_
+    }
+  }
+
+  hidden CreateWebBinding() {
+    $this.log.Info("Creating new IIS Web Binding for website $($this.IISWebSite) on port $($this.tlsPort) with certificate $($this.thumbprint)")
+    try {
+      $this.log.info("Creating new binding")
+      New-WebBinding -Name $($this.IISWebSite) -IP '*' -Port $($this.tlsPort) -Protocol "https"
+    } catch {
+      $this.log.Error("An error occured while creating https binding in IIS")
+      $this.log.error("Error message: $($_.Exception.Message))")
+      $this.log.Error("Error exception: $($_.Exception))")
+      Throw $_
+    }
+    try {
+      $this.log.info("Adding SSL Certificate to binding")
+      (Get-WebBinding -Name $($this.IISWebSite) -Port $($this.tlsPort) -Protocol "https").AddSslCertificate($($this.thumbprint), "MY")
+    } catch {
+      $this.log.Error("An error occured while Adding SSL Certificate to binding")
+      $this.log.Error("Error message: $($_.Exception.Message))")
+      $this.log.Error("Error exception: $($_.Exception))")
+      Throw $_
+    }
+  }
+}
+
+Class CxWsResolverConfigurer : Base {
+  hidden [String] $fqdn
+  hidden [String] $port
+  CxWsResolverConfigurer([String] $fqdn, [String] $port) {
+    $this.fqdn = $fqdn
+    $this.port = $port
+  }
+
+  Configure() {
+    [CheckmarxSystemInfo] $cx = [CheckmarxSystemInfo]::new()
+    if (!(Test-Path $cx.WebConfigFile)) {
+      $this.log.Warn("Checkmarx Web Portal web.config at $($cx.WebConfigFile) does not exist and cannot be updated.")
+      return
+    }
+  
+    $this.log.Info("Updating the Checkmarx Web Portal web.config $($cx.WebConfigFile) CxWSResolver.CxWSResolver key for ssl")
+    [Xml]$xml = Get-Content $cx.WebConfigFile
+    $obj = $xml.configuration.appSettings.add | where {$_.Key -eq "CxWSResolver.CxWSResolver" }
+    $obj.value = "https://$($this.fqdn):$($this.port)/Cxwebinterface/CxWSResolver.asmx"
+    $xml.Save($cx.WebConfigFile)
+    log "... Finished"
+  }
+}
+
+Class CxManagerTlsConfigurer : Base {
+  [String] $tlsPort
+  [bool] $ignoreTlsCertificateValidationErrors
+
+  CxManagerTlsConfigurer([String] $tlsPort, [bool] $ignoreTlsCertificateValidationErrors) {
+    $this.tlsPort = $tlsPort
+    $this.ignoreTlsCertificateValidationErrors = $ignoreTlsCertificateValidationErrors
+  }
+
+  Configure() {
+    [CheckmarxSystemInfo] $cx = [CheckmarxSystemInfo]::new()
+
+    $this.ConfigureTransportSecurity($cx.SystemManagerConfigFile)
+    $this.ConfigureTransportSecurity($cx.JobsManagerConfigFile)
+    $this.ConfigureTransportSecurity($cx.ScansManagerConfigFile)
+    $this.ConfigureWebConfig($cx.WebConfigFile)
+
+    if ($this.ignoreTlsCertificateValidationErrors) {
+      $this.ConfigureIgnoreCertErr($cx.SystemManagerConfigFile)
+      $this.ConfigureIgnoreCertErr($cx.JobsManagerConfigFile)
+      $this.ConfigureIgnoreCertErr($cx.ScansManagerConfigFile)
+      $this.ConfigureIgnoreCertErr($cx.WebConfigFile)
+      $this.ConfigureIgnoreCertErr($cx.WebServicesConfigFile)
+      $this.ConfigureIgnoreCertErr($cx.RestAPIConfigFile)
+    }
+  }
+
+  hidden ConfigureWebConfig([string] $configfile){
+    if (-Not [System.IO.File]::Exists($configfile)) {
+      $this.log.Info("ConfigureWebConfig: $configfile does not exist - assuming it is not installed")
+      return
+    }
+    $this.log.Info("ConfigureWebConfig: Configuring Web Portal resolver on $configfile")    
+    #Update appSettings WebResolver
+    [Xml]$xml = Get-Content $configfile
+    $obj = $xml.configuration.appSettings.add | where {$_.Key -eq "CxWSResolver.CxWSResolver" }
+    $hostAddress = $obj.value
+    $hostAddress = $hostAddress.Replace("http:", "https:").Replace(":80", ":" + $($this.tlsPort)).Replace("localhost", $($this.names.ipaddress))
+    $obj.value = $hostAddress
+    $xml.Save($configfile)
+}
+
+  hidden ConfigureTransportSecurity([String] $configfile) {
+    if (-Not [System.IO.File]::Exists($configfile)) {
+      $this.log.Info("ConfigureTransportSecurity: $configfile does not exist - assuming it is not installed")
+      return
+    }
+    $this.log.Info("Configuring transport security on $configfile")
+
+    [Xml]$xml = Get-Content $configfile
+    $securityNode = Select-Xml -xml $xml -XPath "/configuration/*/bindings/basicHttpBinding/binding/security"
+    if (!$securityNode){
+      $this.log.Error("ERROR: <transport> not found, cannot configure transport security")
+      return
+    }
+    $securityNode.Node.SetAttribute("mode", "Transport")
+    $xml.Save($configfile)
+  }
+
+  hidden ConfigureIgnoreCertErr([String] $configfile) {
+    if (-Not [System.IO.File]::Exists($configfile)) {
+      $this.log.Info("ConfigureIgnoreCertErr: $configfile does not exist - assuming it is not installed")
+      return
+    }
+    $this.log.Info("Configuring ignore cert validation errors on $configfile")
+
+    # Todo, add a check for if system.net w/ settings/spm already exists and update if it does. Suspect this adds multiple system.net blocks if run more than once which is invalid.
+    [xml]$xml = Get-Content $configfile
+    $system = $xml.CreateElement("system.net")
+    $settings = $xml.CreateElement("settings")
+    $spm = $xml.CreateElement("servicePointManager")
+    $spm.SetAttribute("checkCertificateName","false")
+    $spm.SetAttribute("checkCertificateRevocationList","false")
+    $settings.AppendChild($spm)
+    $system.AppendChild($settings)
+    $xml.configuration.AppendChild($system)
+    $xml.Save($configfile)
+  }
+}
--- scripts/ssl/configure-ssl.ps1
+++ scripts/ssl/configure-ssl.ps1
@@ -21,149 +21,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 $ProgressPreference = "SilentlyContinue"
 function log([string] $msg) { Write-Host "$(Get-Date) [$PSCommandPath] $msg" }
 
-function ConfigureIIS($thumbprint) {
-    # Create an ssl binding in SSL and add cert to it
-    log "Creating a new web binding for https"
-    New-WebBinding -Name "Default Web Site" -Protocol "https" -Port 443 -SslFlags 1 -HostHeader *
-    log "Adding certificate to https web binding"
-    (Get-WebBinding -Name "Default Web Site" -Port 443 -Protocol "https").AddSslCertificate($thumbprint, "My")
-    log "Removing http / port 80 web binding"
-    Remove-WebBinding -Name "Default Web Site" -Port 80 -Protocol "http"
-}
-
-function ConfigureWSResolver([string] $domainname) {
-    # Search for the web portal web.config file
-    $cx_web_webconfig = ("{0}\web\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\CheckmarxWebPortal' -Name "Path" -ErrorAction SilentlyContinue), 'web.config')
-    if ((Test-Path "$cx_web_webconfig")) {
-      log "Updating the Checkmarx Web Portal web.config CxWSResolver.CxWSResolver key for ssl"
-      [Xml]$xml = Get-Content $cx_web_webconfig
-      $obj = $xml.configuration.appSettings.add | where {$_.Key -eq "CxWSResolver.CxWSResolver" }
-      $obj.value = "https://${domainname}:443/Cxwebinterface/CxWSResolver.asmx"
-      $xml.Save($cx_web_webconfig)
-      log "... Finished"
-    }
-}
-
-function UpdateHostsFile([string] $domainname) {
-    # Update the hosts file so the manager talks to itself without going through any network infrastructure overhead that might exist at the fqdn
-    $cx_manager_config = ("{0}\bin\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Jobs Manager' -Name "Path" -ErrorAction SilentlyContinue), 'CxJobsManagerWinService.exe.config')
-    if ((Test-Path "$cx_manager_config")) {
-      log "Updating the hosts file to resolve $domainname to 127.0.0.1 to avoid load balancer round trips for web-to-services web-services"
-      "# Checkmarx will resolve the servername to localhost to bypass load balancer hops for inner-app communication" | Add-Content -PassThru "$env:windir\system32\drivers\etc\hosts"
-      "127.0.0.1 $domainname" | Add-Content -PassThru "$env:windir\system32\drivers\etc\hosts"
-      log "... Finished"
-    }
-}
-
-
-function ConfigureManagerServicesTransportSecurity() {
-    # Find all the manager/web service's config files that have been installed and loop over them to configure transport security.
-    @(
-      ("{0}\bin\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx System Manager' -Name "Path" -ErrorAction SilentlyContinue), 'CxSystemManagerService.exe.config'),
-      ("{0}\bin\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Jobs Manager' -Name "Path" -ErrorAction SilentlyContinue), 'CxJobsManagerWinService.exe.config'),
-      ("{0}\bin\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Scans Manager' -Name "Path" -ErrorAction SilentlyContinue), 'CxScansManagerWinService.exe.config'),
-      ("{0}\web\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\CheckmarxWebPortal' -Name "Path" -ErrorAction SilentlyContinue), 'web.config'),
-      ("{0}\CxWebInterface\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Web Services' -Name "Path" -ErrorAction SilentlyContinue), 'web.config'),
-      ("{0}\CxRestAPI\{1}" -f (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Web Services' -Name "PathRestAPI" -ErrorAction SilentlyContinue), 'web.config')
-    ) | ForEach-Object {
-        # Guard clause on existance
-        log "Checking if $_ exists"
-        if ((Test-Path "$_")) {
-            log "Found $_"
-        } else {
-            log "Skipping file that doesn't exist: $_"
-            continue
-        }
-
-        # Set transport security mode
-        [Xml]$xml = Get-Content "$_"
-        log "Adding security mode = Transport"
-        $bindingNode = Select-Xml -xml $xml -XPath "/configuration/*/bindings/basicHttpBinding/binding" 
-        $securityNode = $bindingNode.Node.SelectSingleNode('security')
-        if (!$securityNode){
-          $securityNode = $xml.CreateElement('security')
-          $bindingNode.Node.AppendChild($securityNode)  | Out-Null
-        }
-        $securityNode.SetAttribute("mode", "Transport")
-        $xml.Save("$_")
-    
-        log "Finished configuring transport security on $_"
-    }
-}
-
-function ConfigureEngineTls([string] $domainname, [string] $thumbprint) {
-    # If there is an engine on the machine - it needs some unique configuration
-    $engine_config = ("{0}\{1}" -f ( Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Engine Server' -Name 'Path' -ErrorAction SilentlyContinue), 'CxSourceAnalyzerEngine.WinService.exe.config')
-    if (!(Test-Path "$engine_config")) {
-      log "No engine was detected on this machine. "
-    } else {
-      log "An engine was detected on this machine. It will be configured for TLS."
-      # Configure netsh cert and url pattern reservation
-      $appid = "{00112233-4455-6677-8899-AABBCCDDEEFF}"
-      $ipport = "0.0.0.0:443"
-      $scratch = Join-Path $env:TEMP $(New-Guid) 
-      md -force $scratch
-      Start-Process "netsh.exe" -ArgumentList "http add sslcert ipport=${ipport} certhash=${thumbprint} appid={$appid}" -Wait -NoNewWindow -RedirectStandardError "${scratch}\netsh.out" -RedirectStandardOutput "${scratch}\netsh.err"
-      log "Log of netsh http add sslcert:"
-      log "netsh.out file:"
-      cat "${scratch}\netsh.out"
-      log "netsh.err file:"
-      cat "${scratch}\netsh.err"    
-
-      Start-Process "netsh.exe" -ArgumentList "http add urlacl url=https://+:443/CxSourceAnalyzerEngineWCF/CxEngineWebServices.svc user=`"NT AUTHORITY\NETWORK SERVICE`"" -Wait -NoNewWindow -RedirectStandardError "${scratch}\netsh.out" -RedirectStandardOutput "${scratch}\netsh.err"
-      log "Log of netsh http add urlacl:"
-      log "netsh.out file:"
-      cat "${scratch}\netsh.out"
-      log "netsh.err file:"
-      cat "${scratch}\netsh.err"          
-
-      # Configure transport,binding,baseaddress
-      [Xml]$xml = Get-Content "$engine_config"
-      log "Adding security mode = Transport"
-      $bindingNode = Select-Xml -xml $xml -XPath "/configuration/*/bindings/basicHttpBinding/binding" 
-      $securityNode = $bindingNode.Node.SelectSingleNode('security')
-      if (!$securityNode){
-        $securityNode = $xml.CreateElement('security')
-        $bindingNode.Node.AppendChild($securityNode)  | Out-Null
-      }
-      $securityNode.SetAttribute("mode", "Transport")
-
-      log "Setting mexHttpsBinding"
-      $mexNode = $xml.SelectSingleNode("/configuration/*/services/service/endpoint[@address='mex']")
-      if (!$mexNode) { log "WARN: \<endpoint address=`"mex`" not found in $($engine_config)" }
-      $mexNode.SetAttribute("binding", "mexHttpsBinding")
-  
-      log "Configuring baseAddress"
-      $hostNode = $xml.SelectSingleNode("/configuration/*/services/service/host/baseAddresses/add")     
-      if (!$hostNode) {log "WARN: \<baseAddresses\>\<add\>... not found!" }
-      [string]$hostAddress = $hostNode.SelectSingleNode("@baseAddress").Value
-      $hostAddress = $hostAddress.Replace("http:", "https:").Replace(":80", ":443").Replace("localhost", $($domainname))
-      $hostNode.SetAttribute("baseAddress", $hostAddress)
-  
-      log "Configuring httpsGetEnabled"
-      $serviceNode = $xml.SelectSingleNode("/configuration/*/behaviors/serviceBehaviors/behavior/serviceMetadata")
-      if (!$serviceNode) { log "WARN: \<serviceMetadata\> not found!" }
-      $serviceNode.SetAttribute("httpsGetEnabled", "true")
-      $serviceNode.RemoveAttribute("httpGetEnabled")
-  
-      log "saving $($engine_config)"
-      $xml.Save($engine_config)
-    }
-}
-
-function ConfigureAccessControl() {
-  log "Configuring Checkmarx Access Control appsettings.json for SSL"
-  $appsettings = "C:\Program Files\Checkmarx\Checkmarx Access Control\appsettings.json"
-  if ((Test-Path $appsettings)) {
-    $s = Get-Content "$appsettings" | ConvertFrom-Json
-    $s.Host.ListenUrls = "https://*:443"
-    $s.Host.ExternalListenUrls = "https://*:443"
-    $s.Host.SslCertificate.Filename = $cert.PfxFile
-    $s.Host.SslCertificate.Password = $cert.PfxPass
-    $s | ConvertTo-Json | Set-Content $appsettings
-  }
-}
-
+. $PSScriptRoot\..\CheckmarxAWS.ps1
 $Secure_String_Pwd = ConvertTo-SecureString $pfxpassword -AsPlainText -Force
 
 if ([String]::IsNullOrEmpty($pfxfile) -and [String]::IsNullOrEmpty($domainname) -and [String]::IsNullOrEmpty($pfxpassword)) {
@@ -196,19 +54,19 @@ if ([String]::IsNullOrEmpty($pfxfile) -or [String]::IsNullOrEmpty($domainname)) 
 # Import the cert to the machine on IIS
 log "Importing the certificate into LocalMachine\My"
 $cert = Import-PfxCertificate -FilePath $pfxfile -CertStoreLocation Cert:\LocalMachine\My -Password $Secure_String_Pwd
+$thumbprint = $cert.Thumbprint
+[CxSASTEngineTlsConfigurer]::New($thumbprint).Configure()
+[CxManagerIisTlsConfigurer]::New("Default Web Site", "443", $thumbprint).Configure()
+[CxWsResolverConfigurer]::New($domainname, "443").Configure()
+[CxManagerTlsConfigurer]::New("443", $True).Configure()
+[CheckmarxSystemInfo] $cx = [CheckmarxSystemInfo]::new()
 
-$isManager = !([String]::IsNullOrEmpty((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\CheckmarxWebPortal' -Name "Path" -ErrorAction SilentlyContinue)))
-$isEngine =  !([String]::IsNullOrEmpty((Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Checkmarx\Installation\Checkmarx Engine Server' -Name "Path" -ErrorAction SilentlyContinue)))
-
-if ($isEngine) {
-  ConfigureEngineTls $domainname $cert.Thumbprint
-}
-if ($isManager) {  
-  ConfigureIIS $domainname $cert.Thumbprint
-  ConfigureWSResolver $domainname
-  UpdateHostsFile $domainname
-  ConfigureManagerServicesTransportSecurity
-  ConfigureAccessControl 
+# Update hosts file
+if ($cx.IsSystemManager) {
+  log "Updating the hosts file to resolve $domainname to 127.0.0.1 to avoid load balancer round trips for web-to-services web-services"
+    "# Checkmarx will resolve the servername to localhost to bypass load balancer hops for inner-app communication" | Add-Content -PassThru "$env:windir\system32\drivers\etc\hosts"
+    "127.0.0.1 $domainname" | Add-Content -PassThru "$env:windir\system32\drivers\etc\hosts"
+    log "... Finished"
 }
 
 try {
