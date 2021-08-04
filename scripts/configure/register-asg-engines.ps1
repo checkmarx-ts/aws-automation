a46d2aed24b5f07988198f0934b5872500a61024
RDR3 <robertrichards84@gmail.com>
2021-08-04 11:03:50 -0400

    update register-asg-engines to 9.3 engine endpoint

--- scripts/configure/register-asg-engines.ps1
+++ scripts/configure/register-asg-engines.ps1
@@ -47,7 +47,8 @@ $api.GetEngines() | ForEach-Object {
     $_.State # "running"
     $min_loc = $_.Tags | where { $_.key -eq "checkmarx:engine:loc:min" } | select -ExpandProperty Value
     $max_loc = $_.Tags | where { $_.key -eq "checkmarx:engine:loc:max" } | select -ExpandProperty Value
-    $engineServer = @{ name=$_.PrivateDnsName; uri="https://$($_.PrivateDnsName)/CxSourceAnalyzerEngineWCF/CxEngineWebServices.svc"; minLoc=$min_loc; maxLoc=$max_loc; isBlocked=$False; maxScans = 2 }
+    #$engineServer = @{ name=$_.PrivateDnsName; uri="https://$($_.PrivateDnsName)/CxSourceAnalyzerEngineWCF/CxEngineWebServices.svc"; minLoc=$min_loc; maxLoc=$max_loc; isBlocked=$False; maxScans = 2 }
+    $engineServer = @{ name=$_.PrivateDnsName; uri="https://$($_.PrivateDnsName):8088"; minLoc=$min_loc; maxLoc=$max_loc; isBlocked=$False; maxScans = 2 }
     $existingEngine = $api.FindEngineIdByName($_.PrivateDnsName)
     if ($null -eq $existingEngine) { 
         Write-Host "Registering engine $($_.name)"
@@ -57,7 +58,8 @@ $api.GetEngines() | ForEach-Object {
     }
   
     # Engine servers should upload their self signed certs to the checkmarx s3 bucket so that we can sync them onto the manager to trust their self signed certs        
-    $engineService = "https://$($_.PrivateDnsName)/CxSourceAnalyzerEngineWCF/CxEngineWebServices.svc"
+    #$engineService = "https://$($_.PrivateDnsName)/CxSourceAnalyzerEngineWCF/CxEngineWebServices.svc"
+    $engineService = "https://$($_.PrivateDnsName):8088"
 
     # IF the cert is not on the machine filesystem yet, then we haven't imported it, so download it from s3 and import.
     if (!(Test-Path -Path "C:\programdata\checkmarx\ssl\certs\$($_.PrivateDnsName).cer")) {
